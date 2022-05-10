94b82c3b087a754f9bb15f4f069db176da897331
WAHoltz <wah.holtz@gmail.com>
2022-05-10 14:59:18 -0400

    DEez Gotamam

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,5 +2,6 @@ public class TestClass {
     public static void main(String[] args) {
         Gottem gottem = new Gottem();
         gottem.tipidis();
+        System.out.println("gottem.tipidis()");
     }
 }
